b00d714305857d4d5189d891c55837c054fce425
songexile <therunningbeta@gmail.com>
2023-05-01 18:37:53 +1200

    check if working for netlify

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "xp.css/dist/xp.css": "/path/to/xp.css/dist/xp.css",
+    },
+  },
   build: {
     rollupOptions: {
-      external: ["xp.css/dist/xp.css"],
+      external: [],
     },
   },
 });
